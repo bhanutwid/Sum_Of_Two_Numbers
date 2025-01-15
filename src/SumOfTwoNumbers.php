0a75054e06b40972ab81ecc981aae8bdccbf9d3d
Bhanu Prakash Bindal <bhanu.bindal@twidpay.com>
2025-01-15 17:09:45 +0530

    Checked for Floating and Negative numbers & Handled for Non Numeric Data

--- src/SumOfTwoNumbers.php
+++ src/SumOfTwoNumbers.php
@@ -1,4 +1,9 @@
 <?php
-function SumOfTwoNumbers($num1, $num2) {
+function SumOfTwoNumbers($num1, $num2) 
+{
+    if (!is_numeric($num1) || !is_numeric($num2)) 
+    {
+        throw new InvalidArgumentException('Both arguments should be numbers');
+    }
     return $num1 + $num2; 
 }
--- tests/TestsSumOfTwoNumbers.php
+++ tests/TestsSumOfTwoNumbers.php
@@ -2,8 +2,23 @@
 use PHPUnit\Framework\TestCase;
 
 require_once dirname(__DIR__) . '/src/SumOfTwoNumbers.php';
-class TestsSumOfTwoNumbers extends TestCase {
-    public function testSumOfTwoIntegerNumbers() {
+class TestsSumOfTwoNumbers extends TestCase 
+{
+    public function testSumOfTwoIntegerNumbers() 
+    {
         $this->assertEquals(5, SumOfTwoNumbers(2, 3)); 
+    }
+    public function testSumWithNegativeNumbers() 
+    {
+        $this->assertEquals(1, SumOfTwoNumbers(-2, 3));
+    }
+    public function testSumWithFloatNumbers() 
+    {
+        $this->assertEquals(6, SumOfTwoNumbers(2.5, 3.5));
+    }
+    function testExceptionOfNonNumericType() 
+    {
+        $this->expectException(InvalidArgumentException::class);
+        SumOfTwoNumbers('b', "$");
     }
 }
